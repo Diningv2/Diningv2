b7edad04d51a009382abae2ccb5af632fb8f77f2
Chris Moore <christopher.v.moore@yale.edu>
2019-03-04 03:42:06 -0500

    Initial React Native boilerplate

---
+++ client/app/redux/actions/ApplicationStateActions.js
@@ -0,0 +1,16 @@
+import * as types from './types';
+
+export function startLoading(message) {
+    return {
+        type: types.APP_START_LOADING,
+        payload: {
+            message: message || "Loading..."
+        }
+    }
+}
+
+export function finishLoading() {
+    return {
+        type: types.APP_FINISH_LOADING
+    }
+}
