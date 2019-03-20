a9e398d78c681108f8fe2f193b5889844c074689
sanatkhurana <sanatkhurana@gmail.com>
2019-03-20 15:03:21 +0530

    Update index.js
    Fixing link for MenusRouter (modified during merge)

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const server = express();
 
 import LocationsRouter from "./routers/LocationsRouter/LocationsRouter";
-import MenusRouter from "./routers/MenusRouter/MenusRouter/MenusRouter";
+import MenusRouter from "./routers/MenusRouter/MenusRouter";
 import MenuItemsRouter from "./routers/MenuItemsRouter/MenuItemsRouter";
 
 const PORT = process.env.PORT || 5000;
