6f72ee60b38acab5e9b145bc13159036c5172ab9
Sanat Khurana <sanatkhurana@gmail.com>
2019-03-21 12:26:29 +0530

    Flattened and added procfile for heroku push

--- client/app/redux/actions/index.js
+++
@@ -1,7 +0,0 @@
-import * as IncrementingActions from './IncrementingActions';
-import * as ApplicationStateActions from './ApplicationStateActions';
-
-export const allActions = Object.assign({},
-    IncrementingActions,         // TODO: Delete these, just for getting started with Redux
-    ApplicationStateActions      // TODO: Delete these, just for getting started with Redux
-);
--- client/app/redux/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const INCREASE_ME = 'INCREASE_ME';                 // TODO: Delete later, just used for getting started with Redux
-export const DECREASE_ME = 'DECREASE_ME';                 // TODO: Delete later, just used for getting started with Redux
-
-export const APP_START_LOADING = 'APP_START_LOADING';     // TODO: Delete later, just used for getting started with Redux
-export const APP_FINISH_LOADING = 'APP_FINISH_LOADING';   // TODO: Delete later, just used for getting started with Redux
