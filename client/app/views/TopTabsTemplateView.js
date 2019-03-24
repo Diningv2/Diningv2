9d8a7241786bd60ae19ae6c891e016034fd9f750
Sanat Khurana <sanatkhurana@gmail.com>
2019-03-24 15:32:51 -0400

    TopTabs added with demo

---
+++ client/app/views/TopTabsTemplateView.js
@@ -0,0 +1,67 @@
+// React/React Native imports
+import React, { Component } from 'react';
+import { Alert, View } from 'react-native';
+
+// Redux imports
+import connectToRedux from '../redux/lib/connectToRedux';
+import sp from '../redux/lib/stateProperties';
+
+// Custom component imports
+import Header from '../components/Header';
+
+// Style library import
+import styles from '../config/styles';
+import BottomTabs from '../components/BottomTabs';
+import TopTabs from '../components/TopTabs';
+
+class TopTabsTemplateView extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleAlert(title, msg){
+        // Works on both iOS and Android
+        Alert.alert(
+          title,
+          msg,
+          [
+            {text: 'OK', onPress: () => console.log(title+' Pressed')}
+          ],
+          {cancelable: false},
+        );
+    }
+
+    f1 = ()=>this.handleAlert('f1', 'Pressed f1');
+    f2 = ()=>this.handleAlert('f2', 'Pressed f2');
+    f3 = ()=>this.handleAlert('f3', 'Pressed f3');
+
+    tabButtons = [
+        {
+          tabName: 'Tab1',
+          function: this.f1
+        },
+        {
+          tabName: 'Tab2',
+          function: this.f2
+        },
+        {
+          tabName: 'Tab3',
+          function: this.f3
+        }
+    ]
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Header title="Template View" />
+                <TopTabs tabButtons={this.tabButtons} />
+                <BottomTabs viewName={'TemplateView'}/>
+            </View>
+        )
+    }
+}
+
+// Connects the exported component to Redux
+// for access to the Redux store
+export default connectToRedux(TopTabsTemplateView, []);
