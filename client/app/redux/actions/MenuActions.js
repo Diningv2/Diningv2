4a649f2f7711b220de6edce97577839fe4198049
Zachary Kreiser <zkreiser@Zacharys-MacBook-Pro.local>
2019-03-24 18:41:58 -0400

    zk-MenuView

---
+++ client/app/redux/actions/MenuActions.js
@@ -0,0 +1,50 @@
+import * as types from './types';
+import { get } from '../../lib/api-utility';
+
+export function getMenus(locationID) {
+
+    // -------- The Request Action --------
+    // Usually called to let the Redux reducer
+    // containing our state data that we should
+    // flip an isLoading variable to "true"
+    // while we wait to do the async API call
+    const request = () => {
+        return { type: types.GET_MENU_INFORMATION_REQUEST }
+    }
+
+    // -------- The Success Action --------
+    // Called if the API call happened successfully 
+    // Passes in what we got from the API call to the Redux store in payload.
+    // Also sets "isLoading" back to false.
+    const success = (menus) => {
+        return { 
+            type: types.GET_MENU_INFORMATION_SUCCESS,
+            payload: {
+                menus: menus
+            }
+        }
+    }
+
+    // -------- The Failure Action --------
+    // Called in the catch() block so Redux knows error occurred + what to do.
+    // Maybe set some stuff back to 'undefined' and "isLoading" back to false
+    const failure = (errorMessage) => {
+        return { 
+            type: types.GET_MENU_INFORMATION_FAILURE,
+            errorMessage: errorMessage
+         }
+    }
+
+    // -------- The actual THUNK! --------
+    // dispatch() is the function that executes
+    // one of the above Redux actions.
+    return async (dispatch) => {
+        dispatch(request()); // tell Redux we're about to make that request
+        try {
+            const menus = await get("/api/menus?location=" + locationID); 
+            dispatch(success(menus)); // If successfull, dispatch it to Redux
+        } catch (e) {
+            dispatch(failure(e.message));
+        }
+    }
+}
---
+++ client/app/views/MenuView.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, TouchableOpacity, ScrollView, Alert } from 'react-native';
+import { Transition } from 'react-navigation-fluid-transitions';
+
+import connectToRedux from '../redux/lib/connectToRedux';
+import sp from '../redux/lib/stateProperties';
+
+import Header from '../components/Header';
+import ListItem from '../components/ListItem';
+import BottomTabs from '../components/BottomTabs';
+import TopTabs from '../components/TopTabs';
+
+import styles from '../config/styles';
+
+class MenuView extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        // TODO: Check which dhall you're in from Redux state and 
+        // pass in appropriate location ID - maybe have global mapping
+        // this.props.getMenus(1); 
+        // Maybe nothing here if taken care of on the onPress before
+    }
+
+ 
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+            
+            { !this.props.menusList.isLoading && 
+                <View>
+                <Header title={this.props.menusList.data.location} /> 
+
+                <Transition appear="bottom">
+                    <ScrollView>
+                        {this.props.menusList.isLoading 
+                            ? <Text>Loading...</Text>
+                            : this.getItemsList() 
+                        }
+                    </ScrollView>
+                </Transition>
+                <BottomTabs viewName={'MenuView'}/>
+                
+                </View>
+            }
+            </View>
+        )
+    }
+
+    getItemsList () {
+        return this.props.menusList.data.today.dinner.map((dish, index) => {
+            return (
+                <TouchableOpacity 
+                    key={dish.name}
+                    
+                    onPress={() => {
+                        // TODO: Set redux state that we're viewing dish.index 
+                        //this.props.getMenus(index+1); // Set redux state with menu for this dHall
+                        //this.props.navigation.navigate('MenuView');
+                        this.props.getMenuItemInformation(dish.itemID);
+                        this.props.navigation.navigate('IngredientsView');
+
+                    }} 
+                >
+                    <ListItem title={dish.name} />
+                </TouchableOpacity>
+            )
+        })
+    }
+}
+
+export default connectToRedux(MenuView, ['menusList']);
