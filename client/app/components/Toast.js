bacd91bbc5f6348ad291d39160377ae0bce2267f
Chris Moore <christopher.v.moore@yale.edu>
2019-04-06 15:42:01 -0400

    client/cvm-notifications (#30)
    * Basic notification receipt works on front end
    
    * Frontend push notifications working on Android
    
    * Addition of toast on App.js
    
    * Notifications can now be pressed to be removed and pass in notification JSON data
    
    * Removed unused imports
    
    * Removed unused imports

---
+++ client/app/components/Toast.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import styles from '../config/styles';
+
+export const Toast = (props) => {
+
+    containerStyle = {
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 50,
+        position: 'absolute',
+        alignItems: 'center',
+        width: '100%',
+        top: 0
+    }
+
+    touchableStyle = {
+        zIndex: 1,
+        width: '100%',
+        height: 200,
+    }
+
+    notificationStyle = {
+        ...styles.container.dropShadow,
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        width: '90%',
+        backgroundColor: 'rgba(255, 255, 255, 0.98)',
+        borderRadius: 20
+    }
+
+    textStyles = {
+        title: {
+            ...styles.font.color.primary,
+            ...styles.font.type.primaryRegular,
+            ...styles.font.size.large
+        },
+        message: {
+            ...styles.font.color.tertiary,
+            ...styles.font.type.primaryRegular,
+        }
+    }
+
+
+
+    return (
+        <View style={containerStyle}>
+            <TouchableWithoutFeedback style={touchableStyle} onPress={props.onPress}>   
+                <View style={notificationStyle}>
+                    <Text style={textStyles.title}>{props.title}</Text>
+                    <Text style={textStyles.message}>{props.message}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        </View>
+    )
+}
+
+export default Toast;
