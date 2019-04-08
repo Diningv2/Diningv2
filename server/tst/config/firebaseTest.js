c6a020c611c560532f2cd242bce48dcc348242ac
Sanat Khurana <sanatkhurana@gmail.com>
2019-04-08 18:53:48 -0400

    Small modification to firebaseTest to accomodate for getFavorites firebase functions

--- server/tst/config/firebaseTest.js
+++ server/tst/config/firebaseTest.js
@@ -2,10 +2,15 @@ import firestore from "../../src/config/firebase/firebaseConfig";
 import * as firebase from "firebase-admin";
 
 export default function firebaseTest() {
+  const docData = {"Mock" : "Data"};
+  const docResult = {
+    data: () => docData, 
+    exists: () => true
+  };
   firestore.doc = jest.fn(docname => {
     return { update, get };
   });
-  const get = jest.fn(() => Promise.resolve({"Mock" : "Data"}));
+  const get = jest.fn(() => Promise.resolve(docResult));
   const update = jest.fn(updateValue => Promise.resolve(true));
   firebase.firestore.FieldValue.arrayUnion = jest.fn(val => {
     return val;
