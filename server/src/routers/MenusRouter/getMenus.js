e06e6d23f8c7dd4141c5a68aaa2ba3afe6d4d015
Sanat Khurana <sanatkhurana@gmail.com>
2019-04-13 17:38:05 -0400

    Get cached data from firebase for today/tomorrow menus. Error checking needed

--- server/src/routers/MenusRouter/getMenus.js
+++ server/src/routers/MenusRouter/getMenus.js
@@ -1,12 +1,37 @@
-import getOneMenu from "./getOneMenu";
-import getAllMenus from "./getAllMenus";
+import firestore from "../../config/firebase/firebaseConfig";
+
+import diningLocations from "../../config/locations";
+import * as constants from "../../config/constants";
 
 export default async function getMenus(query) {
-    if (!("location" in query) || query.location == "all") {
-        const menus = await getAllMenus(query);
-        return menus;
-    } else {
-        const menu = await getOneMenu(query);
-        return menu;
+	const locations = (!query.location || query.location == "all") 
+		? Object.values(diningLocations) 
+		: [diningLocations[query.location]];
+	console.log(`Locations ${locations}`)
+
+	let todayDoc = undefined;
+    let tomorrowDoc = undefined;
+    try {
+        todayDoc = await firestore.doc("menus/today").get();
+        tomorrowDoc = await firestore.doc("menus/tomorrow").get();
+    } catch (e) {
+        throw new Error(constants.E_DB_READ + e);
     }
+    if (!todayDoc.exists) {
+        throw new Error(constants.E_DB_NOENT + "menus/today");
+    } else if (!tomorrowDoc.exists) {
+        throw new Error(constants.E_DB_NOENT + "menus/tomorrow");
+    } else if (!query) {
+        throw new Error(constants.E_BAD_FAVE_GET_REQ);
+    }
+    var menu = [];
+    locations.forEach(location => {
+    	// TODO: Deal with errors here
+    	var menuEntry = {};
+    	menuEntry["location"] = location;
+    	menuEntry["today"] = todayDoc.data()[location];
+    	menuEntry["tomorrow"] = tomorrowDoc.data()[location];
+    	menu.push(menuEntry);
+    });
+    return menu;
 }
