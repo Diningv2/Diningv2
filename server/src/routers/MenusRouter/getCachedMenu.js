a9b7a7fd0ce36da99ecb8d777846f4a195f40683
Zeb Mehring <zeb.mehring@yale.edu>
2019-04-17 12:45:52 -0400

    Modified getMenus to pass Firestore docs to getCachedMenu

--- server/src/routers/MenusRouter/getCachedMenu.js
+++ server/src/routers/MenusRouter/getCachedMenu.js
@@ -1,4 +1,4 @@
-export default async function getCachedMenu(location) {
+export default async function getCachedMenu(location, todayDoc, tomorrowDoc) {
     // TODO: Sanat will implement everything from here onwards
     // Note: Should return undefined if the cache data is stale
     return undefined;
--- server/src/routers/MenusRouter/getMenus.js
+++ server/src/routers/MenusRouter/getMenus.js
@@ -1,7 +1,10 @@
+import firestore from "../../config/firebase/firebaseConfig";
+
 import getOneMenu from "./getOneMenu";
 import getCachedMenu from "./getCachedMenu";
 
 import locations from "../../config/locations";
+import { E_DB_READ } from "../../config/constants";
 
 export default async function getMenus(query) {
     const queries =
@@ -12,9 +15,17 @@ export default async function getMenus(query) {
     var nErrors = 0;
     var menus = [];
     for (let location of queries) {
+        var todayDoc = undefined;
+        var tomorrowDoc = undefined;
+        try {
+            todayDoc = await firestore.doc("menus/today").get();
+            tomorrowDoc = await firestore.doc("menus/tomorrow").get();
+        } catch (e) {
+            console.error(`${E_DB_READ}: ${e}`);
+        }
         try {
             const cachedMenu = query.cached
-                ? await getCachedMenu(location)
+                ? await getCachedMenu(location, todayDoc, tomorrowDoc)
                 : undefined;
             const menu = cachedMenu || (await getOneMenu(location));
             menus.push(menu);
