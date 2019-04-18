faa3d3111aebc792ac06773511eff422e013c37e
Zeb Mehring <zeb.mehring@yale.edu>
2019-04-18 08:59:08 -0400

    Updated rollover to set the timestamp on today's menus properly

--- server/src/cron/cleanup/rolloverCache.js
+++ server/src/cron/cleanup/rolloverCache.js
@@ -1,20 +1,29 @@
 import firestore from "../../config/firebase/firebaseConfig";
 
-import * as constants from "../../config/constants";
+import { E_DB_READ, E_DB_NOENT } from "../../config/constants";
+import locations from "../../config/locations";
 
 export default async function rolloverCache() {
     let tomorrowDoc = undefined;
     try {
         tomorrowDoc = await firestore.doc("menus/tomorrow").get();
     } catch (e) {
-        throw new Error(constants.E_DB_READ + e);
+        throw new Error(E_DB_READ + e);
     }
     if (!tomorrowDoc.exists) {
-        throw new Error(constants.E_DB_NOENT + "menus/tomorrow");
+        throw new Error(E_DB_NOENT + "menus/tomorrow");
     }
     try {
-        await firestore.doc("menus/today").set(tomorrowDoc.data());
-        await firestore.doc("menus/tomorrow").set(constants.emptyDoc);
+        const date = new Date();
+        const timestamp = date.toDateString();
+        var newTodayDoc = {};
+        Object.values(locations).forEach(location => {
+            newTodayDoc[location] = {
+                menu: tomorrowDoc.data()[location].menu,
+                timestamp
+            };
+        });
+        await firestore.doc("menus/today").set(newTodayDoc);
     } catch (e) {
         console.error(e);
     }
