6c2a304e2d2345eb53b12d518b83d5c1e2a6af30
sanatkhurana <sanatkhurana@gmail.com>
2019-04-22 15:22:05 -0400

    server/sk-getFavoritesCached (#70)
    * getFavorites with caching
    
    * Format change and timestamp check
    
    * Refactored caching folder
    
    * Meal strings formatted and isBeingServed flag added

---
+++ server/src/routers/CacheFavoritesRouter/CacheFavoritesRouter.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import cacheFavorites from "../../cron/caching/Favorites/cacheFavorites";
+
+import { E_BAD_MENU_REQ } from "../../config/constants";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const cachedFavoritesToday = await cacheFavorites();
+        res.send("Caching today's menuitems...").status(200);
+    } catch (e) {
+        console.error(e);
+        if (e.message == E_BAD_MENU_REQ) {
+            res.send(e).status(400);
+        } else {
+            res.send(e).status(500);
+        }
+    }
+});
+
+export default router;
