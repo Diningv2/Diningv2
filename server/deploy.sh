4c205f4da7d9a41a68e355f399cbd5e5335be8a2
Zeb Mehring <zeb.mehring@yale.edu>
2019-04-22 19:22:59 -0400

    Added a tentative script to deploy to heroku

---
+++ server/deploy.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+DATE=$(date '+%m/%d/%y-%H.%M.%S')
+READABLE_DATE=$(date '+%m/%d/%y %H:%M:%S')
+
+git checkout master;
+git checkout -b heroku-$DATE;
+rm -rf client/*;
+mv src/* ./*;
+rmdir src;
+rm -f yarn.lock;
+npm install && npm audit fix;
+npm run build;
+git commit -a -m "Heroku deployment for ${READABLE_DATE}";
+# TODO: fix these lines
+git push -f heroku master
