c96f75d5e085965e92a93682e99d5cc617714892
Chris Moore <christopher.v.moore@yale.edu>
2019-04-23 10:12:28 -0400

    FavoritesView now uses Dish.js (#77)

--- client/app/views/Favorites/FavoritesListAll.js
+++ client/app/views/Favorites/FavoritesListAll.js
@@ -8,6 +8,7 @@ import styles from '../../config/styles';
 import connectToRedux from '../../redux/lib/connectToRedux';
 import sp from '../../redux/lib/stateProperties';
 import Hint from '../../components/Hint';
+import Dish from '../../components/Dish';
 
 const FavoritesListAll = (props) => {
 
@@ -15,19 +16,21 @@ const FavoritesListAll = (props) => {
         // Because the backend response schema for getFavorites is different than the
         // redux store response when you favorite something immediately in the app.
         const dishName = props.favoritesList.data[dishID].name || props.favoritesList.data[dishID]
-
+            const dish = {
+                name: dishName,
+                itemID: dishID,
+                hasInfo: false
+            };
         return (
             <AnimatedListItem key={dishID}>
-                <View style={{...styles.container.spaceBelowSmall}}>
-                    <FavoriteItemCard dishID={dishID} name={dishName} />
-                </View>
+                <Dish dish={dish} />
             </AnimatedListItem>
         );
     }
 
     return (
             <View>
-                <Hint message="These are all the dishes you've favorited. Hold down on one to remove it." />
+                <Hint message="These are all the dishes you've favorited." />
                 <DV2ScrollView 
                     array={Object.keys(props.favoritesList.data)}
                     render={(dishID) => renderFavesList(dishID)}
