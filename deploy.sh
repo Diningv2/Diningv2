951d71e4e3781f9d789462ea33c667ed6b630bae
Sanat Khurana <sanatkhurana@gmail.com>
2019-04-23 12:40:04 -0400

    Minor changes

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,15 @@ mv server/* .;
 rmdir server;
 rm -f yarn.lock;
 sed -i '' -e 's/^diningv2-4a304/# &/' ./.gitignore;
-read -p "Press [Enter] key to start backup...";
+read -p "Check for presence of secret file. Press [Enter] to continue...";
 npm install;
 npm run build;
-git commit -a -m "Heroku deployment for ${READABLE_DATE}";
-read -p "Delete old endpoints if > 5. Press [Enter] key to start backup...";
-heroku login
-heroku create
-read -p "Change name to serverv2 on heroku, after changing current serverv2. Press [Enter] key to start backup...";
-heroku git:remote -a serverv2
+git add --all;
+git commit -m "Heroku deployment for ${READABLE_DATE}";
+read -p "Delete old endpoints if > 5. Press [Enter] to continue...";
+read -p "Change name to serverv2 on heroku, after changing current serverv2. Press [Enter] to continue...";
+heroku login;
+heroku create;
+heroku git:remote -a serverv2;
 git push heroku heroku-$DATEmaster;
 git checkout master;
