b3e06f6af322644c87c816b57d55081df9601216
Sanat Khurana <sanatkhurana@gmail.com>
2019-04-23 12:28:43 -0400

    Modified script

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,20 @@ READABLE_DATE=$(date '+%m/%d/%y %H:%M:%S')
 
 git checkout master;
 git checkout -b heroku-$DATE;
-rm -rf client/*;
-mv src/* ./*;
-rmdir src;
+rm -r client/*;
+rm -r node_modules;
+mv server/* .;
+rmdir server;
 rm -f yarn.lock;
-sed -i '/^diningv2-4a304/d' ./.gitignore;
-npm install && npm audit fix;
+sed -i '' -e 's/^diningv2-4a304/# &/' ./.gitignore;
+read -p "Press [Enter] key to start backup...";
+npm install;
 npm run build;
 git commit -a -m "Heroku deployment for ${READABLE_DATE}";
-# TODO: fix these lines
-git push -f heroku master;
+read -p "Delete old endpoints if > 5. Press [Enter] key to start backup...";
+heroku login
+heroku create
+read -p "Change name to serverv2 on heroku, after changing current serverv2. Press [Enter] key to start backup...";
+heroku git:remote -a serverv2
+git push heroku heroku-$DATEmaster;
 git checkout master;
