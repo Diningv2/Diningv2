c0fd591ef4787c0f24192d2a84567ce5936e2de2
Zeb Mehring <zeb.mehring+github@gmail.com>
2019-04-23 08:52:21 -0400

    server/zm-populateMenusGranularity

--- server/src/cron/caching/Menus/populateMenus.js
+++ server/src/cron/caching/Menus/populateMenus.js
@@ -1,7 +1,7 @@
-import * as firebase from "firebase-admin";
-import firestore from "../../../config/firebase/firebaseConfig";
+import firestore from "../../config/firebase/firebaseConfig";
 
 import getOneMenu from "./getOneMenu";
+import mergeMenu from "./mergeMenu";
 
 import locations from "../../../config/locations";
 import { E_DB_WRITE } from "../../../config/constants";
@@ -13,18 +13,26 @@ export default async function populateMenus() {
     for (let location in locations) {
         const menu = await getOneMenu(location);
         try {
-            menu &&
+            const mergedMenu =
+                menu &&
                 menu.today &&
-                (await firestore.doc("menus/today").update({
-                    [menu.location]: { menu: menu.today, timestamp }
-                }));
-            menu &&
                 menu.tomorrow &&
+                (await mergeMenu(menu, location));
+            mergedMenu &&
+                mergedMenu.today &&
+                (await firestore.doc("menus/today").update({
+                    [mergedMenu.location]: { menu: mergedMenu.today, timestamp }
+                }));
+            mergedMenu &&
+                mergedMenu.tomorrow &&
                 (await firestore.doc("menus/tomorrow").update({
-                    [menu.location]: { menu: menu.tomorrow, timestamp }
+                    [mergedMenu.location]: {
+                        menu: mergedMenu.tomorrow,
+                        timestamp
+                    }
                 }));
         } catch (e) {
-            console.error(E_DB_WRITE + e);
+            console.error(`${E_DB_WRITE}: ${e}`);
         }
     }
     console.log("Finished populating Firestore.");
