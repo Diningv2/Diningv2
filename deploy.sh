e25cd339d2cde1ec4129b672b75a99e0e632e7c6
Zeb Mehring <zeb.mehring+github@gmail.com>
2019-04-23 16:47:12 -0400

    server/zm-preferencesFixes
    * Fixed erroneous database accesses in addPreference
    
    * Fixed mismatched imports and added extra checks to preference adding

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,15 @@ READABLE_DATE=$(date '+%m/%d/%y %H:%M:%S')
 
 git checkout master;
 git checkout -b heroku-$DATE;
-rm -r client/*;
-rm -r node_modules;
+rm -rf client/*;
+rm -rf node_modules;
 mv server/* .;
 rmdir server;
 rm -f yarn.lock;
 sed -i '' -e 's/^diningv2-4a304/# &/' ./.gitignore;
 read -p "Add secret json file if missing. Press [Enter] to continue...";
 npm install;
+npm audit fix;
 npm run build;
 git add --all;
 git commit -m "Heroku deployment for ${READABLE_DATE}";
